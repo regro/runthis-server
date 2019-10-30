d5dc3a33bd6e2dc723b15907e5b8baa4cd2b6d26
Anthony Scopatz <scopatz@gmail.com>
2019-10-30 00:50:12 -0500

    some fixes

--- runthis_server/main.py
+++ runthis_server/main.py
@@ -1,23 +1,26 @@
 import asyncio
+from itertools import count
 
 from quart import Quart, redirect, request
 
 
 app = Quart("runthis-server")
-
+cnt = count(8080)
 procs = {}
 
 
 @app.route('/start', methods=['GET', 'POST'])
 async def start():
+    port = str(next(cnt))
     proc = await asyncio.create_subprocess_shell(
-        " ".join(["/home/scopatz/Downloads/t/gotty", "-w", "--once", "-p", "8080", "xonsh"]),
+        " ".join(["/home/scopatz/Downloads/t/gotty", "-w", "--once", "-p", port, "xonsh"]),
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE)
     data = await request.get_data()
     print(data)
-    procs["8080"] = proc
-    return redirect("http://0.0.0.0:8080")
+    procs[port] = proc
+    print(procs)
+    return redirect("http://0.0.0.0:" + port)
 
 
 @app.after_serving
