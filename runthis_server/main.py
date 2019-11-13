eb6efc7daeeac7bfb26ede657a8cf0158f2e991d
Anthony Scopatz <scopatz@gmail.com>
2019-11-13 18:33:12 -0500

    minor fix for GET requests

--- runthis_server/main.py
+++ runthis_server/main.py
@@ -51,6 +51,7 @@ def get_subprocess_command(data):
     args = []
     port = str(next(cnt))
     d = None
+    data = {} if data is None else data
     # Apply tty server
     if conf.tty_server == "gotty":
         args.extend([conf.gotty_path, "-w", "--once", "-p", port])
