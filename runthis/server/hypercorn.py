bd1770c065522194d1c4276718073740cb12c049
Anthony Scopatz <scopatz@gmail.com>
2020-03-19 12:44:35 -0500

    hypercorn initial entry point

---
+++ runthis/server/hypercorn.py
@@ -0,0 +1,17 @@
+"""Hypercorn main entry point"""
+from . import main
+
+
+if os.path.isfile("runthis-server.yml"):
+    main.conf = main.get_config_from_yaml("runthis-server.yml")
+else:
+    print("No config file found!")
+    main.conf = Config()
+main.lang = main.find_lang(main.conf)
+
+# start up server
+print(main.conf)
+main._setup_redirect_base(main.conf.host)
+cnt = count(main.conf.tty_server_port_start)
+
+app = main.app
